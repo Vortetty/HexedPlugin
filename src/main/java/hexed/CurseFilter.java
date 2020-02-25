ee7c233c25ae07ed70ca8fc5747b9f324ec15e78
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-02-25 07:48:47 -0800

    Update CurseFilter.java

--- src/main/java/hexed/CurseFilter.java
+++ src/main/java/hexed/CurseFilter.java
@@ -1,7 +1,8 @@
 package hexed;
 
-//TODO put in other plugin
+// TODO put in other plugin
 public class CurseFilter{
-    //this is probably the most curses I've ever typed
-    public static final String[] swears = {"shit", "fuck", "asshole", "nigger", "nigga", "faggot", "dick", "bitch"};
+    // this is probably the most curses I've ever typed
+    public static final String[] swears = {/*"shit", "fuck", "asshole", "nigger", "nigga", "faggot", "dick", "bitch"*/};
+    // yeah... lets just disable that filter - vortetty
 }
